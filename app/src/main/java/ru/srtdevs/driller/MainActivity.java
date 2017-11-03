14a53a393719e04e416d73ef97a9b027c3c47191
vitek999 <vitek2012rus@gmail.com>
2017-11-04 01:22:57 +0300

    use SingleFragmentActivity in MainActivity

--- app/src/main/java/ru/srtdevs/driller/MainActivity.java
+++ app/src/main/java/ru/srtdevs/driller/MainActivity.java
@@ -1,13 +1,10 @@
 package ru.srtdevs.driller;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends SingleFragmentActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        return new SelectActionFragment();
     }
 }
