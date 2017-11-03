81300dd519cafe3adb8a44dbc561e92207a6a303
vitek999 <vitek2012rus@gmail.com>
2017-11-04 00:54:42 +0300

    add Buttons to SelectActionFragment

--- app/src/main/java/ru/srtdevs/driller/SelectActionFragment.java
+++ app/src/main/java/ru/srtdevs/driller/SelectActionFragment.java
@@ -5,12 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by виктор on 04.11.2017.
  */
 
 public class SelectActionFragment extends Fragment {
+    private Button mNewProjectButton;
+    private Button mOpenProjectButton;
+    private Button mLoadProjectButton;
+    private Button mAppSettingsButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,12 @@ public class SelectActionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.select_action_fragment, container, false);
+
+        mNewProjectButton = (Button) v.findViewById(R.id.new_project_btn);
+        mOpenProjectButton = (Button) v.findViewById(R.id.open_project_btn);
+        mLoadProjectButton = (Button) v.findViewById(R.id.load_project_btn);
+        mAppSettingsButton = (Button) v.findViewById(R.id.app_settings_btn);
+
         return v;
     }
 }
