5d374ceeed604dcaee74bfc594e5fda3d56d02b2
vitek999 <vitek2012rus@gmail.com>
2017-11-04 00:44:16 +0300

    add SelectActionFragment

---
+++ app/src/main/java/ru/srtdevs/driller/SelectActionFragment.java
@@ -0,0 +1,24 @@
+package ru.srtdevs.driller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by виктор on 04.11.2017.
+ */
+
+public class SelectActionFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.select_action_fragment, container, false);
+        return v;
+    }
+}
