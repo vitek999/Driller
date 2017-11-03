8deaa8eea24640206869e3a2606b49f829819872
vitek999 <vitek2012rus@gmail.com>
2017-11-04 01:14:48 +0300

    create SingleFragmentActivity

---
+++ app/src/main/java/ru/srtdevs/driller/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package ru.srtdevs.driller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by виктор on 04.11.2017.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity{
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if(fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
