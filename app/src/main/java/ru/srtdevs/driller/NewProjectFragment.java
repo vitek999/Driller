6ee99dda7a3d8913d2102480dcb90a01a05f7b5b
vitek999 <vitek2012rus@gmail.com>
2017-11-04 10:13:23 +0300

    include Buttons and EditText to NewProjectFragment

--- app/src/main/java/ru/srtdevs/driller/NewProjectFragment.java
+++ app/src/main/java/ru/srtdevs/driller/NewProjectFragment.java
@@ -5,12 +5,23 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by виктор on 04.11.2017.
  */
 
 public class NewProjectFragment extends Fragment {
+    private Button mEnterButton;
+    private Button mBackButton;
+    private EditText mShortTitleEditText;
+    private EditText mFullTitleEditText;
+    private EditText mArchiveNumberEditText;
+    private EditText mRegionEditText;
+    private EditText mNoteEditText;
+    private EditText mStartDateEditText;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +30,15 @@ public class NewProjectFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.new_project_fragment, container, false);
+
+        mEnterButton = (Button) v.findViewById(R.id.enter_btn);
+        mBackButton = (Button) v.findViewById(R.id.back_btn);
+        mShortTitleEditText = (EditText) v.findViewById(R.id.short_title_et);
+        mFullTitleEditText = (EditText) v.findViewById(R.id.full_title_et);
+        mArchiveNumberEditText = (EditText) v.findViewById(R.id.archive_number_et);
+        mRegionEditText = (EditText) v.findViewById(R.id.region_et);
+        mNoteEditText = (EditText) v.findViewById(R.id.note_et);
+        mStartDateEditText = (EditText) v.findViewById(R.id.start_date_et);
 
         return v;
     }
